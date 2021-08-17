c1d3b90e84fc280e9a42d708eb7e7c9873f89743
Jose JABG. Antonio Belchior Gomes <josegomes@gac.local>
2021-08-17 11:06:45 -0300

    fix: changed to opacity and fix total time

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const initialDate = new Date(`2021-08-13T12:00:00.000Z`).getTime();
     const now = new Date().getTime();
     var dif = Math.floor((date.getTime() - now) / 1000);
-    const totalTime = dif;
+    const totalTime = (finalDate.getTime() - initialDate) / 1000;
     const elapsedTime = Math.floor((now - initialDate) / 1000);
     const days = Math.floor(dif / 86400);
     dif = dif % 86400;
@@ -17,7 +17,10 @@ export default function Home() {
     dif = dif % 60;
     const seconds = dif;
     const countDownText = dif >= 0 ? `${days.toString().padStart(2, '0')}:${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}` : `00:00:00:00`
-    const incrementalSaturation = elapsedTime <= totalTime ? 100 - ((elapsedTime / totalTime) * 100) : 0;
+    const incrementalSaturation = elapsedTime <= totalTime ? ((elapsedTime / totalTime) * 100) : 0;
+
+    console.log(elapsedTime)
+    console.log(totalTime)
 
     return {
               countDownText,
@@ -26,7 +29,7 @@ export default function Home() {
   }
 
   const finalDate = new Date('2021-09-03T20:00:00.000Z')
-  //const finalDate = new Date('2021-08-21T20:00:00.000Z')
+  //const finalDate = new Date('2021-08-18T20:00:00.000Z')
   const { countDownText, incrementalSaturation } = runCountdown(finalDate);
   const [countDown, setCountDown] = useState(countDownText);
   const [saturation, setSaturation] = useState(incrementalSaturation);
@@ -36,6 +39,7 @@ export default function Home() {
     setTimeout(() => {
       const { countDownText, incrementalSaturation } = runCountdown(finalDate);
       setCountDown(countDownText)
+      console.log(saturation)
       setSaturation(incrementalSaturation)
       if (countDownText === `00:00:00:00`) {
         if(audioPlayer) audioPlayer.current.play();
@@ -66,13 +70,13 @@ export default function Home() {
            <div className={styles['photo-grid']}>
               <div className={styles.layers}>
                 <img className={styles.backphoto} src="images/photo.png" alt="" />
-                <img style={{ filter: `saturate(${saturation}%)` }} className={styles.jairo} src="images/jairo.png" alt="" />
-                <img style={{ filter: `saturate(${saturation}%)` }} className={styles.ze} src="images/ze.png" alt="" />
+                <img style={{ opacity: `${saturation}%` }} className={styles.jairo} src="images/jairo.png" alt="" />
+                <img style={{ opacity: `${saturation}%` }} className={styles.ze} src="images/ze.png" alt="" />
               </div>
 
               <div className={styles.layers}>
                 <img className={styles.backphoto2} src="images/photo2.png" alt="" />
-                <img style={{ filter: `saturate(${saturation}%)` }} className={styles.alef} src="images/alef.png" alt="" />
+                <img style={{ opacity: `${saturation}%` }} className={styles.alef} src="images/alef.png" alt="" />
               </div>
             </div>
             <audio src="assets/Mariarita.mp3" id="my_audio" loop={true} ref={audioPlayer}></audio>
