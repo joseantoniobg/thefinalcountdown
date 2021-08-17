85147716816a3c73061323c6795edb7f237c3eae
Jose JABG. Antonio Belchior Gomes <josegomes@gac.local>
2021-08-17 13:06:49 -0300

    solved some problems at app js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,14 +5,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-            <title>The final countdown</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
-            <link href="/images/icon.png" rel="icon" />
-        </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
+        <link href="/images/icon.png" rel="icon" />
+        <Head title="The final countdown"></Head>
         <body>
           <Main />
           <NextScript />
